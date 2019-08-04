27224c6ec26ee10834efef613541e6438f853ba3
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-08-04 15:54:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < musicians.length; i++) 
@@ -26,3 +27,12 @@ function iLoveTheBeatles(number) {
   } while (number < 15);
   return array
 }
+=======
+// add solution here
+function theBeatlesPlay(musician, instrument) {
+  return `${musician} plays ${instrument}`
+}
+
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Base Guitar", "Lead Guitar", "Drums"]
+>>>>>>> 2b14040b8e8b9ddcf8bcf7274c9056cd068ad123
